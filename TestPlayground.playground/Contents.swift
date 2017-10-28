ab470cd105f3733d58fb963112fd798a56b82806
Sam Showalter <samwam2@gmail.com>
2017-10-27 21:45:25 -0700

    worked on design and practice xcode playground

---
+++ TestPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var hi = "hi"
+var calendarDateToday = "October 27"
+var date = 9
+
+var testArray = [1, 4, 6]
+
+let name = "sam"
+
+str = "Frick IA"
+
+calendarDateToday = "October 28th"
+
+func test() {
+    
+}
+
+
+
