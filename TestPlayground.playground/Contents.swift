4d58718138f9bb80e3050ad03ba283b4b65dcba5
Sam Showalter <samwam2@gmail.com>
2017-11-06 16:59:55 -0800

    New test swift file for code and updated the deign file for the app.

--- TestPlayground.playground/Contents.swift
+++ TestPlayground.playground/Contents.swift
@@ -2,23 +2,18 @@
 
 import UIKit
 
-var str = "Hello, playground"
-
-var hi = "hi"
-var calendarDateToday = "October 27"
-var date = 9
-
-var testArray = [1, 4, 6]
-
-let name = "sam"
-
-str = "Frick IA"
-
-calendarDateToday = "October 28th"
-
-func test() {
+class testStuff {
+    //Vars
     
+    var samIsCool = true
+    
+    samIsCool
+    
+  
+    
+    
+    
+  //Last class closing bracket
 }
-
 
 
